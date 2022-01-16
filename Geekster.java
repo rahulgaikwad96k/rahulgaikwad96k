35a3b4121bb6e659f43eab1dfc952f6dfe703504
Rahul Gaikwad <97580362+rahulgaikwad96k@users.noreply.github.com>
2022-01-16 15:56:08 +0530

    Add files via upload
    Precourse Assignment geekster

---
+++ Geekster.java
@@ -0,0 +1,24 @@
+package JavaPrecourseAssignment;
+
+public class Geekster {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 21;
+
+		if (a > b && a > c) {
+			System.out.println("largest number is " + a);
+
+		}
+		if (b > a && b > c) {
+			System.out.println("largest number is " + b);
+
+		}
+		if (c > a && c > b) {
+			System.out.println("largest number is " + c);
+
+		}
+	}
+
+}
